ee58b1fe0c1d23d2102bc9d0ead4f23528705cc4
sergiojami <semache87@hotmail.com>
2026-08-12 16:35:37 +0200

    Update Supabase key in config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 window.AVIONICA_SUPABASE_URL = "https://ftvtcwoksgzopofayibe.supabase.co";
-window.AVIONICA_SUPABASE_KEY = "TU_CLAVE_PUBLICA";
+window.AVIONICA_SUPABASE_KEY = "sb_publishable_CYAyshe2mgJb7XwlAUNLbg_UKQC9F1j";
