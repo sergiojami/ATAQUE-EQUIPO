53a3f05e15e3c72e9ca69070f5c35ff988e61b47
sergiojami <semache87@hotmail.com>
2026-08-12 17:38:21 +0200

    Restore working Supabase connection

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-window.AVIONICA_SUPABASE_URL = "https://ftvtcwoksgzopofayibe.supabase.co";
-window.AVIONICA_SUPABASE_KEY = "sb_publishable_CYAyshe2mgJb7XwlAUNLbg_UKQC9F1j";
+window.AVIONICA_SUPABASE_URL = "https://buqcamwpfumqymbzcokj.supabase.co";
+window.AVIONICA_SUPABASE_KEY = "sb_publishable_SK69C_3j5dW8dYLTGMAuog_O06gBCOe";
